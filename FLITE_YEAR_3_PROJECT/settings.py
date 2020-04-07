1c89254cb39d3f522ff1634ed370f0ef48a7534a
ianodonnell0891 <47224050+ianodonnell0891@users.noreply.github.com>
2020-04-07 22:00:04 +0100

    Third Commit

--- FLITE_YEAR_3_PROJECT/settings.py
+++ FLITE_YEAR_3_PROJECT/settings.py
@@ -134,4 +134,16 @@ CRISPY_TEMPLATE_PACK = 'bootstrap4'
 LOGIN_REDIRECT_URL = 'newsfeed-home'
 LOGIN_URL = 'login'
 
-GOOGLE_MAPS_API_KEY = 'AIzaSyC86jOoP3Exe7NeVWmgckjT0Cwf8owy6OI'
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = os.environ.get('USER_EMAIL')
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_PASSWORD')
+
+
+
+
+
+
+
